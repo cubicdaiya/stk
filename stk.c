3b6291dd6d7f926565e144da16e2b6d8d8bf8780
cubicdaiya <cubicdaiya@gmail.com>
2010-04-28 20:43:55 +0900

    change indent size

--- stk.c
+++ stk.c
@@ -1,35 +1,35 @@
 /**
-In short, stk is distributed under so called "BSD license",
-
-Copyright (c) 2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
- * Redistributions of source code must retain the above copyright notice, 
+   In short, stk is distributed under so called "BSD license",
+   
+   Copyright (c) 2010 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+   All rights reserved.
+   
+   Redistribution and use in source and binary forms, with or without modification, 
+   are permitted provided that the following conditions are met:
+   
+   * Redistributions of source code must retain the above copyright notice, 
    this list of conditions and the following disclaimer.
-
- * Redistributions in binary form must reproduce the above copyright notice, 
+   
+   * Redistributions in binary form must reproduce the above copyright notice, 
    this list of conditions and the following disclaimer in the documentation 
    and/or other materials provided with the distribution.
-
- * Neither the name of the authors nor the names of its contributors 
+   
+   * Neither the name of the authors nor the names of its contributors 
    may be used to endorse or promote products derived from this software 
    without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+   
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /* written by C99 style */
 
@@ -39,45 +39,45 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * create vector
  */
 stk_t *stk_create(size_t capasiz, size_t elemsiz) {
-  stk_t *vec;
-  STK_MALLOC(vec, sizeof(stk_t));
-  if (capasiz <= 0) {
-    capasiz = STK_DEFALT_SIZE;
-  }
-  if (elemsiz <= 0) {
-    elemsiz = STK_DEFALT_ELEM_SIZE;
-  }
-  vec->capa = capasiz;
-  vec->siz = 0;
-  vec->elemsiz = elemsiz;
-  STK_MALLOC(vec->arr, elemsiz * capasiz);
-  return vec;
+    stk_t *vec;
+    STK_MALLOC(vec, sizeof(stk_t));
+    if (capasiz <= 0) {
+        capasiz = STK_DEFALT_SIZE;
+    }
+    if (elemsiz <= 0) {
+        elemsiz = STK_DEFALT_ELEM_SIZE;
+    }
+    vec->capa = capasiz;
+    vec->siz = 0;
+    vec->elemsiz = elemsiz;
+    STK_MALLOC(vec->arr, elemsiz * capasiz);
+    return vec;
 }
 
 /**
  * push an element to vector
  */
 bool stk_push(stk_t *vec, const void *elem) {
-  if (vec->siz >= vec->capa) {
-    uchar_t *tmp = realloc(vec->arr, (vec->elemsiz * vec->capa) * 2);
-    if (tmp == NULL) {
-      return false;
+    if (vec->siz >= vec->capa) {
+        uchar_t *tmp = realloc(vec->arr, (vec->elemsiz * vec->capa) * 2);
+        if (tmp == NULL) {
+            return false;
+        }
+        vec->arr = tmp;
+        vec->capa *= 2;
     }
-    vec->arr = tmp;
-    vec->capa *= 2;
-  }
-  uchar_t *arr_idx = (uchar_t *)STK_IDXP(vec, vec->siz);
-  memcpy(arr_idx, elem, vec->elemsiz);
-  vec->siz++;
-  return true;
+    uchar_t *arr_idx = (uchar_t *)STK_IDXP(vec, vec->siz);
+    memcpy(arr_idx, elem, vec->elemsiz);
+    vec->siz++;
+    return true;
 }
 
 /**
  * release vector
  */
 void stk_destroy(stk_t *vec) {
-  STK_FREE(vec->arr);
-  STK_FREE(vec);
+    STK_FREE(vec->arr);
+    STK_FREE(vec);
 }
 
 
--- test/stk_test.c
+++ test/stk_test.c
@@ -8,65 +8,65 @@ static void stk_test002(void);
 static void stk_test003(void);
 
 static void stk_test001(void) {
-  stk_t *vec = stk_create(0, sizeof(int));
-  int arr[] = {1,2,3,4,5,6,7,8,9,10};
-  int siz = sizeof(arr) / sizeof(*arr);
-  for (int i=0;i<siz;++i) {
-    stk_push(vec, &arr[i]);
-  }
-  CU_ASSERT(STK_SIZ(vec) == 10);
-  for (int i=0;i<siz;++i) {
-    CU_ASSERT(STK_IDX(vec, i) == i+1);
-  }
-  stk_destroy(vec);
+    stk_t *vec = stk_create(0, sizeof(int));
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int siz = sizeof(arr) / sizeof(*arr);
+    for (int i=0;i<siz;++i) {
+        stk_push(vec, &arr[i]);
+    }
+    CU_ASSERT(STK_SIZ(vec) == 10);
+    for (int i=0;i<siz;++i) {
+        CU_ASSERT(STK_IDX(vec, i) == i+1);
+    }
+    stk_destroy(vec);
 }
 
 static void stk_test002(void) {
-  stk_t *vec = stk_create(5, sizeof(char));
-  char arr[] = "abcdefghi";
-  int siz = sizeof(arr) / sizeof(*arr) - 1;
-  for (int i=0;i<siz;++i) {
-    stk_push(vec, &arr[i]);
-  }
-  CU_ASSERT(STK_SIZ(vec) == 9);
-  for (int i=0;i<siz;++i) {
-    CU_ASSERT(STK_IDX(vec, i) == arr[i]);
-  }
-  stk_destroy(vec);
+    stk_t *vec = stk_create(5, sizeof(char));
+    char arr[] = "abcdefghi";
+    int siz = sizeof(arr) / sizeof(*arr) - 1;
+    for (int i=0;i<siz;++i) {
+        stk_push(vec, &arr[i]);
+    }
+    CU_ASSERT(STK_SIZ(vec) == 9);
+    for (int i=0;i<siz;++i) {
+        CU_ASSERT(STK_IDX(vec, i) == arr[i]);
+    }
+    stk_destroy(vec);
 }
 
 static void stk_test003(void) {
-  typedef struct test_s {
-    int n;
-    char c;
-  } test_t;
-  stk_t *vec = stk_create(5, sizeof(test_t));
-  test_t arr[10];
-  int siz = sizeof(arr) / sizeof(arr[0]);
-  for (int i=0;i<siz;++i) {
-    arr[i].n = i + 1;
-    arr[i].c = 'a' + i;
-    stk_push(vec, &arr[i]);
-  }
-  CU_ASSERT(STK_SIZ(vec) == 10);
-  for (int i=0;i<siz;++i) {
-    test_t *t = (test_t *)STK_IDXP(vec, i);
-    CU_ASSERT(t->n == i + 1);
-    CU_ASSERT(t->c == 'a' + i);
-  }
-  stk_destroy(vec);
-  
+    typedef struct test_s {
+        int n;
+        char c;
+    } test_t;
+    stk_t *vec = stk_create(5, sizeof(test_t));
+    test_t arr[10];
+    int siz = sizeof(arr) / sizeof(arr[0]);
+    for (int i=0;i<siz;++i) {
+        arr[i].n = i + 1;
+        arr[i].c = 'a' + i;
+        stk_push(vec, &arr[i]);
+    }
+    CU_ASSERT(STK_SIZ(vec) == 10);
+    for (int i=0;i<siz;++i) {
+        test_t *t = (test_t *)STK_IDXP(vec, i);
+        CU_ASSERT(t->n == i + 1);
+        CU_ASSERT(t->c == 'a' + i);
+    }
+    stk_destroy(vec);
+    
 }
 
 
 int main (int argc, char *argv[]) {
-  CU_pSuite suite_stk;
-  CU_initialize_registry();
-  suite_stk = CU_add_suite("stk", NULL, NULL);
-  CU_add_test(suite_stk, "stk_test001", stk_test001);
-  CU_add_test(suite_stk, "stk_test002", stk_test002);
-  CU_add_test(suite_stk, "stk_test003", stk_test003);
-  CU_basic_run_tests();
-  CU_cleanup_registry();
-  return 0;
+    CU_pSuite suite_stk;
+    CU_initialize_registry();
+    suite_stk = CU_add_suite("stk", NULL, NULL);
+    CU_add_test(suite_stk, "stk_test001", stk_test001);
+    CU_add_test(suite_stk, "stk_test002", stk_test002);
+    CU_add_test(suite_stk, "stk_test003", stk_test003);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return 0;
 }
